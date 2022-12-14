57a4a891826f134282ea51e766f4f1e99bd87089
Edwin Benalcázar <Edwin.Benalcazar@rocketrez.com>
2022-12-14 16:20:16 -0500

    add taskForm component

--- src/app/components/TaskForm.js
+++ src/app/components/TaskForm.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const TaskForm = () => {
-  return (
-    <div>TaskForm</div>
-  )
-}
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+  });
 
-export default TaskForm
+  const handleChange = (e) => {
+    setTask({ ...task, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(task);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="title"
+        type="text"
+        placeholder="title"
+        onChange={handleChange}
+      />
+      <textarea
+        name="description"
+        placeholder="description"
+        onChange={handleChange}
+      />
+      <button>Save</button>
+    </form>
+  );
+};
+
+export default TaskForm;
--- src/app/components/TaskList.js
+++ src/app/components/TaskList.js
@@ -1,22 +1,19 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-
+import React from "react";
+import { useSelector } from "react-redux";
 
 const TaskList = () => {
-    const tasks = useSelector((state)=> state.tasks);
-    console.log(tasks);
-    return (
-        <div>
-            {
-            tasks.map(
-                (task) => (
-                <div key={task.id}>
-                    <h3>{task.title} </h3>
-                    <p>{task.description}</p>
-                </div>
-            ))}
+  const tasks = useSelector((state) => state.tasks);
+  console.log(tasks);
+  return (
+    <div>
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <h3>{task.title}</h3>
+          <p>{task.description}</p>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default TaskList
+export default TaskList;
